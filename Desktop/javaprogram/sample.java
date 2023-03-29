f2d6b03be75404151a5a497feceed1bf07266a4b
sankhu <sankhu.d@gmail.com>
2023-03-29 06:57:28 +0530

    added basic java program

---
+++ Desktop/javaprogram/sample.java
@@ -0,0 +1,13 @@
+public class sample {
+
+	public static void main(String[] args) {
+		
+		
+		for (int i=0;i<10;i++)
+			{
+				System.out.println("hello how are you? how long you will look for job!");
+			}
+
+	}
+
+}

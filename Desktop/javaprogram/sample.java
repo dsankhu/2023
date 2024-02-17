72ac78fb79e5cd5cfc433f718afbff181fb4d3f0
dsankhu <46552931+dsankhu@users.noreply.github.com>
2024-02-17 20:51:33 +0530

    Update sample.java

--- Desktop/javaprogram/sample.java
+++ Desktop/javaprogram/sample.java
@@ -3,7 +3,7 @@ public class sample {
 	public static void main(String[] args) {
 		
 		
-		for (int i=0;i<11;i++)
+		for (int i=0;i<12;i++)
 			{
 				System.out.println("hello how are you? how long you will look for job!");
 			}
